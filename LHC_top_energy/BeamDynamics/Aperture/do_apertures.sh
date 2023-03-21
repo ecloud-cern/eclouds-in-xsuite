14f8e2873f7f01092fd8c5f7c25bf52eb4e61801
kparasch <konstnatinos.paraschou@cern.ch>
2023-03-21 13:24:39 +0100

    apertures

---
+++ LHC_top_energy/BeamDynamics/Aperture/do_apertures.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+conda activate xsuite
+python aperture.py --ecloud_type q1r5
+python aperture.py --ecloud_type dr12r5
+python aperture.py --ecloud_type q2ar5
+python aperture.py --ecloud_type dr2r5
+python aperture.py --ecloud_type q2br5
+python aperture.py --ecloud_type dr23r5
+python aperture.py --ecloud_type q3r5
+
+python aperture.py --ecloud_type q1l5
+python aperture.py --ecloud_type dr12l5
+python aperture.py --ecloud_type q2al5
+python aperture.py --ecloud_type dr2l5
+python aperture.py --ecloud_type q2bl5
+python aperture.py --ecloud_type dr23l5
+python aperture.py --ecloud_type q3l5
+
+python aperture.py --ecloud_type q1r1
+python aperture.py --ecloud_type dr12r1
+python aperture.py --ecloud_type q2ar1
+python aperture.py --ecloud_type dr2r1
+python aperture.py --ecloud_type q2br1
+python aperture.py --ecloud_type dr23r1
+python aperture.py --ecloud_type q3r1
+
+python aperture.py --ecloud_type q1l1
+python aperture.py --ecloud_type dr12l1
+python aperture.py --ecloud_type q2al1
+python aperture.py --ecloud_type dr2l1
+python aperture.py --ecloud_type q2bl1
+python aperture.py --ecloud_type dr23l1
+python aperture.py --ecloud_type q3l1
