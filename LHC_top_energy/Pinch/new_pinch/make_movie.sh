153321059cd29eb7fc9178b56ae4cb8a80e871c1
kparasch <konstantinos.paraschou@cern.ch>
2024-01-23 18:28:13 +0100

    remove old new pinch

--- LHC_top_energy/Pinch/new_pinch/make_movie.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-simname=pinch_test
-ffmpeg -i frames/frame_%d.png -c:v libx264 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2,setpts=1.*PTS" -profile:v high -level:v 4.0 -pix_fmt yuv420p -crf 22  -codec:a aac pinch_test.mp4
