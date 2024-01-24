9f5beb735af9c2fd127da7de0585a1f345094e2e
kparasch <konstantinos.paraschou@cern.ch>
2024-01-24 10:10:54 +0100

    plot nel from eos

--- LHC_top_energy/Buildup/analysis/plot_nel.py
+++ LHC_top_energy/Buildup/analysis/plot_nel.py
@@ -3,7 +3,9 @@ import scipy.io
 import matplotlib.pyplot as plt
 import json
 
-folder = "/home/kparasch/workspace/eclouds-in-xsuite/LHC_top_energy/Buildup/simulations/LHC6.8TeV_v1_Q1R5_0_sey1.35_1.20e11ppb_3x48/"
+folder = "/eos/project/e/ecloud-simulations/kparasch/LHC_Triplets/Buildup/run3_bet30cm_160urad_1.2e11ppb_2.0um/simulations_SEY1.35/" \
+       + "LHC6.8TeV_v1_DR23R5_5_sey1.35_1.20e11ppb/"
+# folder = "/home/kparasch/workspace/eclouds-in-xsuite/LHC_top_energy/Buildup/simulations/LHC6.8TeV_v1_Q1R5_0_sey1.35_1.20e11ppb_3x48/"
 
 sim = folder.split("/")[-2]
 ecloud_type = sim.split("_")[2]
